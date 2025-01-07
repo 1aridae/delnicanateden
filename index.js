0e32bb2d376d80036adb16a87f7448b064fe59b4
1aridae <ivetrih@hotmail.com>
2025-01-07 14:42:13 +0100

    html js update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var startDate = new Date("2024-09-02");
 var today = Date.now();
 
-var nlbr = 127.5;
-var krka = 139.0;
+var nlbr = 127.0;
+var krka = 139.5;
 
 function trenutniTeden() {
   var diff = today - startDate;
@@ -15,7 +15,7 @@ function trenutniTeden() {
 }
 
 function vrednostPortfelja() {
-  var vrednost = 10 * nlbr + 8 * krka;
+  var vrednost = 10 * nlbr + 9 * krka;
   var vrednostText = vrednost.toLocaleString("de-DE", {
     minimumFractionDigits: 1,
     maximumFractionDigits: 1,
@@ -81,10 +81,10 @@ const stocks = [
   },
   {
     name: "KRKA",
-    startingPrice: 138.31,
+    startingPrice: 138.44,
     endingPrice: krka,
-    shares: 8,
-    costs: 11.20,
+    shares: 9,
+    costs: 12.60,
     dividends: 0,
   },
 ];
